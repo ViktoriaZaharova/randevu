ca00955e5e7d6d5b14f7c4466e86fd2d70c694f2
Viktoria <zaharova181114@gmail.com>
2022-04-06 19:49:37 +0300

    add modal

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,5 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.latest-articles-slider').slick({
+    slidesToShow: 4,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
