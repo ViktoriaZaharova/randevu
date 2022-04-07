01e3c5efc3f3e65264bf13edbde6a67e3f4b8cf7
Viktoria <zaharova181114@gmail.com>
2022-04-07 23:21:33 +0300

    index

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,67 @@
 $('.latest-articles-slider').slick({
     slidesToShow: 4,
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 576) {
+        $('.cities-slider:not(.slick-initialized)').slick({
+            dots: false,
+            slidesToShow: 1,
+            arrows: true,
+            prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+            nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+        });
+    } else {
+        $(".cities-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
+
+// password view
+$('.btn-view-password').on('click', function(){
+    if ($(this).siblings('.input-password').attr('type') === 'password'){
+        $(this).addClass('view');
+        $(this).siblings('.input-password').attr('type', 'text');
+    } else {
+        $(this).removeClass('view');
+        $(this).siblings('.input-password').attr('type', 'password');
+    }
+    return false;
+});
+
+$('header .location-item').on('click', function (e) {
+    e.preventDefault();
+    $('header .location-cities').fadeToggle();
+});
+
+$('.mobile-menu .location-item').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu .location-cities').fadeToggle();
+});
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
 });
